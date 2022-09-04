6bbd6243269e94672f3241f498cfd78bf5ca2c5e
Kritarth_IIITD <89209652+Kritarth384@users.noreply.github.com>
2022-09-04 20:23:27 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,4 @@
 /* eslint-disable prettier/prettier */
 // eslint-disable-next-line prettier/prettier
 export const ip = 'https://eipsinsight.herokuapp.com'
-<<<<<<< HEAD
 // export const ip = 'http://localhost:5000'
-=======
->>>>>>> 5e2d3b16ac07d8ba0b30f1520c09ede1dad94b6a
